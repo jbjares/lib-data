99cad1627c04868251881dd167fcd54d1c28e669
Lukas Zimmermann <luk.zim91@gmail.com>
2018-09-20 14:25:45 +0200

    Release: Version 0.0.4

---
+++ src/test/kotlin/de/difuture/ekut/pht/lib/data/DockerContainerOutputTests.kt
@@ -0,0 +1,30 @@
+package de.difuture.ekut.pht.lib.data
+
+import org.junit.Test
+
+/**
+ * Tests [DockerContainerOutput]
+ *
+ * @author Lukas Zimmermann
+ * @since 0.0.4
+ */
+class DockerContainerOutputTests {
+
+    private val testIds = assertAllEqual {
+        DockerContainerOutput(
+                DockerContainerId(it),
+                0,
+                "",
+                "").containerId.repr
+    }
+
+    @Test
+    fun valid_container_outputs_no_prefix() {
+        testIds(false)
+    }
+
+    @Test
+    fun valid_container_outputs_with_prefix() {
+        testIds(true)
+    }
+}
