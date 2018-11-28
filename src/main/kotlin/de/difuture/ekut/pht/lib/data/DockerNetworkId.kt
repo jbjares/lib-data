e2b5800f9c393baa70b3ad4f5d08076d5772eb8f
Lukas Zimmermann <luk.zim91@gmail.com>
2018-11-28 09:39:12 +0100

    0.0.9: Update Kotlin Version

--- src/main/kotlin/de/difuture/ekut/pht/lib/data/DockerNetworkId.kt
+++ src/main/kotlin/de/difuture/ekut/pht/lib/data/DockerNetworkId.kt
@@ -5,13 +5,16 @@ import de.difuture.ekut.pht.lib.internal.requireIsValidDockerHash
 /**
  * Represents the ID from a Docker Network.
  *
- * The [String] representation from instances must be a a valid Docker network ID.
+ * The [String] representation from instances must be a valid Docker network ID. The network Id can only
+ * be understood in the context of the underlying Docker daemon.
+ *
+ * Note that this class can likely be refactored to a Kotlin inline class once this feature
+ * has become stable.
  *
  * @author Lukas Zimmermann
  * @since 0.0.1
  */
 data class DockerNetworkId(val repr: String) {
-
     init {
         requireIsValidDockerHash(repr, DockerNetworkId::class.java)
     }
