9237c120031924cff101fa4b4111344ec54ac888
Lukas Zimmermann <luk.zim91@gmail.com>
2018-12-05 15:32:09 +0100

    Release 0.1.1: Move code to upstream lib

--- src/main/kotlin/de/difuture/ekut/pht/lib/data/DockerImageId.kt
+++ src/main/kotlin/de/difuture/ekut/pht/lib/data/DockerImageId.kt
@@ -5,11 +5,11 @@ import de.difuture.ekut.pht.lib.internal.requireIsValidDockerHash
 /**
  * Represents the ID of a Docker Image.
  *
- * The [String] representation from instances must be a valid Docker image ID. The image Id can only
+ * The [String] representation of instances must be a valid Docker image ID. The image Id can only
  * be understood in the context of the underlying Docker daemon.
  *
  * Note that this class can likely be refactored to a Kotlin inline class once this feature
- * has become stable.
+ * has become stable (which is not the case in Kotlin 1.3.10).
  *
  * @author Lukas Zimmermann
  * @since 0.0.1
